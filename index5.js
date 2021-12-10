389307c6f15467e9f0a0caff9c415db7e2c219aa
AxlidnovJ <jamshidbekaxlidinovtatu@gmail.com>
2021-12-10 12:05:52 +0500

    add

--- index5.js
+++ index5.js
@@ -1,11 +1,22 @@
 function qosh() {
     var a = document.getElementById("bir").value;
     var b = document.getElementById("ikki").value;
-    var hisob = document.getElementById("hisob");
-    hisob.value = Number(a) + Number(b);
+    var hisob = document.getElementById("hisob").value;
+    var ok = document.getElementById("reja");
 
+    if (Number(hisob) == Number(a) + Number(b)) {
+        ok.classList.add("true");
+        ok.innerHTML = "Togri";
+        ok.classList.remove("false");
+    } else {
+        ok.classList.add("false");
+        ok.innerHTML = "xato";
+        ok.classList.remove("true");
 
+    }
+}
 
-
-
+function tozala() {
+    var ok = document.getElementById("reja");
+    ok.innerHTML = "";
 }
