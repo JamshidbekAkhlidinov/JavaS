a1a97c06df49a9d9879829afbdca16192d918f5d
AxlidnovJ <jamshidbekaxlidinovtatu@gmail.com>
2021-12-10 11:21:27 +0500

    add files

---
+++ index5.js
@@ -0,0 +1,11 @@
+function qosh() {
+    var a = document.getElementById("bir").value;
+    var b = document.getElementById("ikki").value;
+    var hisob = document.getElementById("hisob");
+    hisob.value = Number(a) + Number(b);
+
+
+
+
+
+}
